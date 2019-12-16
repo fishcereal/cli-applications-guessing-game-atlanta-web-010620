f906959904dea6e027a1f44efb5c112f184552bd
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-16 15:11:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,13 @@
 def run_guessing_game 
-    num = 3
-    
-    puts num
-    p num 
+    num = rand(5)+1
+    # num = 5
+    input = gets.chomp
+    puts "input is #{input}"
+    if input.to_i == num 
+        puts "You guessed the correct number!"
+    elsif input == 'exit'
+        puts "Goodbye!"
+    else 
+        puts "Sorry! The computer guessed #{num}."
+    end
 end
-
